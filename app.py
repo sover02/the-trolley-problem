3bec8ddc2498b5ebea04011dbf472e12bff0073a
David Anderson <david@misplaced.us>
2024-10-23 23:16:13 -0600

    great falls -> missoula

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import streamlit.components.v1 as components
-st.title("Montana Day Trips \n City-to-City Ticket Booking")
+st.title("🚌 Daytrip® \n City-to-City Ticket Booking")
 # List of cities
-cities = ["Bozeman", "Great Falls", "Missoula"]
+cities = ["Bozeman", "Billings", "Missoula"]
 
 # Dropdown for 'From' city
 from_city = st.selectbox("From:", cities)
@@ -16,23 +16,23 @@ if st.button("Book Tickets"):
         st.success(f"Tickets booked from {from_city} to {to_city}!")
         if from_city == "Missoula" and to_city == "Bozeman":
             components.iframe("https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d2825286.638113635!2d-115.1550279294301!3d46.248856423346794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x535dcc2a50f367cb%3A0xe9e31277ca94802e!2sMissoula%2C%20Montana!3m2!1d46.8721284!2d-113.9940314!4m5!1s0x5345444c4fba8813%3A0x63f5d064f73b60aa!2sBozeman%2C%20MT!3m2!1d45.679311899999995!2d-111.03725899999999!5e0!3m2!1sen!2sus!4v1729718007070!5m2!1sen!2sus")
-            st.write("Bozeman is a great city! Your ride will leave at __:__ on __/__/__. Enjoy your trip!")
+            st.write("Bozeman is a great city! Your ride will leave at 09:30 on 11/05/2024. Enjoy your trip!")
         elif from_city == "Bozeman" and to_city == "Missoula":
             components.iframe("https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d2825281.749719902!2d-115.15502791927071!3d46.24895132802401!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x5345444c4fba8813%3A0x63f5d064f73b60aa!2sBozeman%2C%20MT!3m2!1d45.679311899999995!2d-111.03725899999999!4m5!1s0x535dcc2a50f367cb%3A0xe9e31277ca94802e!2sMissoula%2C%20Montana!3m2!1d46.8721284!2d-113.9940314!5e0!3m2!1sen!2sus!4v1729721814965!5m2!1sen!2sus")
-            st.write("Missoula is a great city! Your ride will leave at __:__ on __/__/__. Enjoy your trip!") 
-        elif from_city == "Bozeman" and to_city == "Great Falls":
-            components.iframe("https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d1403936.9730217694!2d-112.71511319846933!3d46.58596424010401!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x5345444c4fba8813%3A0x63f5d064f73b60aa!2sBozeman%2C%20MT!3m2!1d45.679311899999995!2d-111.03725899999999!4m5!1s0x5342378d658cb83b%3A0xd6de56b18e5945a8!2sGreat%20Falls%2C%20MT!3m2!1d47.5052849!2d-111.3007715!5e0!3m2!1sen!2sus!4v1729722292227!5m2!1sen!2sus")
-            st.write("Great Falls is a great city! Your ride will leave at __:__ on __/__/__. Enjoy your trip!")
-        elif from_city == "Great Falls" and to_city == "Bozeman":
-            components.iframe("https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d1403223.074881477!2d-112.7330103782887!3d46.61352285527773!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x5342378d658cb83b%3A0xd6de56b18e5945a8!2sGreat%20Falls%2C%20MT!3m2!1d47.5052849!2d-111.3007715!4m5!1s0x5345444c4fba8813%3A0x63f5d064f73b60aa!2sBozeman%2C%20MT!3m2!1d45.679311899999995!2d-111.03725899999999!5e0!3m2!1sen!2sus!4v1729722586832!5m2!1sen!2sus")
-            st.write("Bozeman is a great city! Your ride will leave at __:__ on __/__/__. Enjoy your trip!")
-        elif from_city == "Great Falls" and to_city == "Missoula":
-            components.iframe("https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d693827.0903553538!2d-113.3081026644887!3d47.211454217955364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x5342378d658cb83b%3A0xd6de56b18e5945a8!2sGreat%20Falls%2C%20MT!3m2!1d47.5052849!2d-111.3007715!4m5!1s0x535dcc2a50f367cb%3A0xe9e31277ca94802e!2sMissoula%2C%20MT!3m2!1d46.8721284!2d-113.9940314!5e0!3m2!1sen!2sus!4v1729723207607!5m2!1sen!2sus")
-            st.write("Missoula is a great city! Your ride will leave at __:__ on __/__/__. Enjoy your trip!")
-        elif from_city == "Missoula" and to_city == "Great Falls":
-            components.iframe("https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d696061.260774003!2d-113.3063774309157!3d47.04044077103771!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x535dcc2a50f367cb%3A0xe9e31277ca94802e!2sMissoula%2C%20MT!3m2!1d46.8721284!2d-113.9940314!4m5!1s0x5342378d658cb83b%3A0xd6de56b18e5945a8!2sGreat%20Falls%2C%20MT!3m2!1d47.5052849!2d-111.3007715!5e0!3m2!1sen!2sus!4v1729723287128!5m2!1sen!2sus")
-            st.write("Great Falls is a great city! Your ride will leave at __:__ on __/__/__. Enjoy your trip!")
+            st.write("Missoula is a great city! Your ride will leave at 14:15 on 11/12/2024. Enjoy your trip!") 
+        elif from_city == "Bozeman" and to_city == "Billings":
+            components.iframe("https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d1426555.1130005287!2d-111.08807952749913!3d45.70621277062569!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x5345444c4fba8813%3A0x63f5d064f73b60aa!2sBozeman%2C%20MT!3m2!1d45.679311899999995!2d-111.03725899999999!4m5!1s0x53486f8888fa9d97%3A0x373556d4f179b550!2sBillings%2C%20MT!3m2!1d45.7832856!2d-108.5006904!5e0!3m2!1sen!2sus!4v1729745874567!5m2!1sen!2sus")
+            st.write("Billings is a great city! Your ride will leave at 11:45 on 11/18/2024. Enjoy your trip!")
+        elif from_city == "Billings" and to_city == "Bozeman":
+            components.iframe("https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d1426548.0695403074!2d-111.08181107177944!3d45.70648877250476!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x53486f8888fa9d97%3A0x373556d4f179b550!2sBillings%2C%20MT!3m2!1d45.7832856!2d-108.5006904!4m5!1s0x5345444c4fba8813%3A0x63f5d064f73b60aa!2sBozeman%2C%20MT!3m2!1d45.679311899999995!2d-111.03725899999999!5e0!3m2!1sen!2sus!4v1729745974376!5m2!1sen!2sus")
+            st.write("Bozeman is a great city! Your ride will leave at 08:00 on 11/23/2024. Enjoy your trip!")
+        elif from_city == "Billings" and to_city == "Missoula":
+            components.iframe("https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d2823189.2360936226!2d-113.88051327537192!3d46.28956218042968!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x53486f8888fa9d97%3A0x373556d4f179b550!2sBillings%2C%20MT!3m2!1d45.7832856!2d-108.5006904!4m5!1s0x535dcc2a50f367cb%3A0xe9e31277ca94802e!2sMissoula%2C%20MT!3m2!1d46.8721284!2d-113.9940314!5e0!3m2!1sen!2sus!4v1729746016733!5m2!1sen!2sus")
+            st.write("Missoula is a great city! Your ride will leave at 10:30 on 11/27/2024. Enjoy your trip!")
+        elif from_city == "Missoula" and to_city == "Billings":
+            components.iframe("https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d2827460.9387272!2d-113.88679068475159!3d46.206628993663166!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x535dcc2a50f367cb%3A0xe9e31277ca94802e!2sMissoula%2C%20MT!3m2!1d46.8721284!2d-113.9940314!4m5!1s0x53486f8888fa9d97%3A0x373556d4f179b550!2sBillings%2C%20MT!3m2!1d45.7832856!2d-108.5006904!5e0!3m2!1sen!2sus!4v1729746076246!5m2!1sen!2sus")
+            st.write("Billings is a great city! Your ride will leave at 13:00 on 11/30/2024. Enjoy your trip!")
     else:
         st.error("Please select different cities for 'From' and 'To'.")
 
-st.write("Here at Montana Day Trips, we are dedicated to providing you with the best travel experience possible. We offer an alternative service to ensure your journey is comfortable and enjoyable. Our team is committed to providing you with the highest level of service throughout your trip. We look forward to helping you explore Montana and create unforgettable memories with others.")
+st.write("Here at Daytrip®, we are dedicated to providing you with the best travel experience possible. We offer an alternative service to ensure your journey is comfortable and enjoyable. Our team is committed to providing you with the highest level of service throughout your trip. We look forward to helping you explore Montana and create unforgettable memories with others.")
