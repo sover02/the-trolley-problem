c38490ebae489f6f9828c20a52c6f88fdd67b961
WilliamSheldon-CS <wjsheldon5@gmail.com>
2024-10-24 02:23:51 -0600

    made price calc for tickets

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import streamlit.components.v1 as components
 st.title("🚌 Daytrip® \n City-to-City Ticket Booking")
-# List of cities
+# List of cities and ticket quantities
 cities = ["Bozeman", "Billings", "Missoula"]
+tickets = list(range(1, 11))
+# Dropdown for 'tickets'
+num_tickets = st.selectbox("Number of tickets:", tickets)
 
 # Dropdown for 'From' city
 from_city = st.selectbox("From:", cities)
@@ -12,8 +15,14 @@ to_city = st.selectbox("To:", cities)
 
 # Button to book tickets
 if st.button("Book Tickets"):
+    if from_city == "Missoula" and to_city == "Bozeman" or from_city == "Bozeman" and to_city == "Missoula":
+        price = 50
+    elif from_city == "Bozeman" and to_city == "Billings" or from_city == "Billings" and to_city == "Bozeman":
+        price = 45
+    elif from_city == "Missoula" and to_city == "Billings" or from_city == "Billings" and to_city == "Missoula":
+        price = 60
     if from_city != to_city:
-        st.success(f"Tickets booked from {from_city} to {to_city}!")
+        st.success(f"{num_tickets} Tickets booked from {from_city} to {to_city}! \n Your total is ${price * num_tickets}.")
         if from_city == "Missoula" and to_city == "Bozeman":
             components.iframe("https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d2825286.638113635!2d-115.1550279294301!3d46.248856423346794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x535dcc2a50f367cb%3A0xe9e31277ca94802e!2sMissoula%2C%20Montana!3m2!1d46.8721284!2d-113.9940314!4m5!1s0x5345444c4fba8813%3A0x63f5d064f73b60aa!2sBozeman%2C%20MT!3m2!1d45.679311899999995!2d-111.03725899999999!5e0!3m2!1sen!2sus!4v1729718007070!5m2!1sen!2sus")
             st.write("Bozeman is a great city! Your ride will leave at 09:30 on 11/05/2024. Enjoy your trip!")
